d814367512d43a79ed52e6f5ce6bede8f9e04f52
Supreeth-Basabattini <supreeth.basabattini@gmail.com>
2021-04-03 12:06:56 +0530

    initial commit

---
+++ src/main/java/com/supreeth/spense/SpenseApplication.java
@@ -0,0 +1,13 @@
+package com.supreeth.spense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpenseApplication.class, args);
+	}
+
+}
